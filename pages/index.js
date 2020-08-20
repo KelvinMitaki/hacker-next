36bf888d42317a3c0e3f2b68fce0eac018caea03
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-20 09:02:06 +0300

    added story details

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Axios from "axios";
 import Error from "next/error";
+import StoryList from "../components/StoryList";
 
 export class index extends Component {
   static async getInitialProps() {
@@ -20,25 +21,7 @@ export class index extends Component {
     return (
       <div>
         <h1>hacker next</h1>
-        {this.props.stories.map(story => (
-          <h2 key={story.id}>{story.title}</h2>
-        ))}
-        <style jsx>{`
-          body {
-            text-align: center;
-            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-              Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
-              sans-serif;
-          }
-        `}</style>
-        <style global jsx>{`
-          body {
-            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-              Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
-              sans-serif;
-            font-weight: bold;
-          }
-        `}</style>
+        <StoryList stories={this.props.stories} />
       </div>
     );
   }
