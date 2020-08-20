129be01df2a4364995ae16cd2bbafe4954d2d42c
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-20 15:27:29 +0300

    added a next webpack service worker config

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+
+module.exports = {
+  webpack: config => {
+    config.plugins.push(
+      new SWPrecacheWebpackPlugin({
+        minify: true,
+        staticFileGlobsIgnorePatterns: [/\.next\//],
+        runtimeCaching: [{ handler: "networkFirst", urlPattern: /^https?.*/ }]
+      })
+    );
+  }
+};
