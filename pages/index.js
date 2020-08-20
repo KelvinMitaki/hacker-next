ab30914637ae03aa06e9eb294f27b6de1f557707
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-20 09:42:01 +0300

    added basic pagination

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,16 @@ import Axios from "axios";
 import Error from "next/error";
 import StoryList from "../components/StoryList";
 import Layout from "../components/Layout";
+import Link from "next/link";
 
 export class index extends Component {
-  static async getInitialProps() {
+  static async getInitialProps({ req, res, query }) {
     try {
+      const page = Number(query.page) || 1;
       const res = await Axios.get(
-        "https://node-hnapi.herokuapp.com/news?page=1"
+        `https://node-hnapi.herokuapp.com/news?page=${page}`
       );
-      return { stories: res.data };
+      return { stories: res.data, page };
     } catch (error) {
       return { statusCode: error.response.status };
     }
@@ -25,6 +27,11 @@ export class index extends Component {
         description="A Hacker News clone built with Next js "
       >
         <StoryList stories={this.props.stories} />
+        <footer>
+          <Link href={`/?page=${this.props.page + 1}`}>
+            <a>Click Here</a>
+          </Link>
+        </footer>
       </Layout>
     );
   }
